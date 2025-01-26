7e73ddad9db103b11564904f33e456745459896c
shrutibel27 <shrutibelakute05@gmail.com>
2025-01-27 01:52:35 +0530

    display list of restrurants using demo dataset

--- ResturantApp-FrontEnd/src/app/screens/restro-list/Components/list/list.component.ts
+++ ResturantApp-FrontEnd/src/app/screens/restro-list/Components/list/list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { RestaurantService } from 'src/app/services/restaurant.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  restaurants:any[]=[];
+  constructor(private restaurantService: RestaurantService){
+    this.restaurants = this.restaurantService.getrestro();
+  }
 
+
+  addrestro(){
+    const newRestaurant = {
+      id: this.restaurants.length + 1,
+      name: 'xyz',
+      location: 'pqr',
+      type: '...',
+    };
+    this.restaurants.push(newRestaurant);
+  }
+  removerestro(id:number){
+    this.restaurants.splice(id-1, 1);
+  }
+  
 }
--- ResturantApp-FrontEnd/src/app/services/restaurant.service.ts
+++ ResturantApp-FrontEnd/src/app/services/restaurant.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class RestaurantService {
 
   constructor() { }
+
+  getrestro(){
+    return[
+      {id:1,name:'gokul',location:'kondhwa',type:'veg'},
+      {id:2,name:'kanha',location:'kondhwa',type:'veg'},
+      {id:3,name:'abhishek',location:'baramati',type:'veg/non-veg'},
+      {id:4,name:'govinda',location:'kondhwa',type:'veg'}
+    ];
+  }
 }
